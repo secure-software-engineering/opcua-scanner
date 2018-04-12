de005f01faba39aeab1c8a6434257ce069d3e2d9
Melanie Bruns <mbruns42@mail.uni-paderborn.de>
2018-04-12 09:27:48 +0200

    Add logging and move to named package

---
+++ src/de/fraunhofer/iem/opcuascanner/ScanningClient.java
@@ -0,0 +1,27 @@
+package de.fraunhofer.iem.opcuascanner;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+//TODO import client sdk from milo
+
+public class ScanningClient{
+
+    private static final Logger logger = LoggerFactory.getLogger(ScanningClient.class);
+
+    public static void main(String[] args){
+        logger.info("Started");
+
+        //TODO get range of ips to check
+        //TODO run client for each
+
+        //TODO second phase: Try to set up connection anonymously
+
+        //TODO second phase: Try to read
+
+        //TODO third phase: Certificate tests, see BSI assessment, table 22, suppressable errors
+
+        // TODO report results
+    }
+}
+
