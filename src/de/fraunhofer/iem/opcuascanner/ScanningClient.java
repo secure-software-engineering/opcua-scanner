c088bdd61af620fce1b148956e145ad2c3f01c07
Melanie Bruns <mbruns42@mail.uni-paderborn.de>
2018-04-20 13:17:51 +0200

    Scan default OPCUA port instead of trying TCP Echo

--- src/de/fraunhofer/iem/opcuascanner/ScanningClient.java
+++ src/de/fraunhofer/iem/opcuascanner/ScanningClient.java
@@ -15,9 +15,9 @@ import java.util.concurrent.ExecutionException;
 
 public class ScanningClient {
 
-    private static final String OPCUA_DEFAULT_PORT = "4840";
-
+    private static final int OPCUA_DEFAULT_PORT = 4840;
     private static final int DEFAULT_CIDR_SUFFIX = 24;
+    private static final int DEFAULT_TIMEOUT_IN_MS = 500;
 
     private static final Logger logger = LoggerFactory.getLogger(ScanningClient.class);
 
@@ -25,15 +25,15 @@ public class ScanningClient {
         logger.info("Scanner started");
 
         List<InetAddress> ownIps = getOwnIpAddresses();
-        for (InetAddress ownIp : ownIps){
+        for (InetAddress ownIp : ownIps) {
             logger.info("Own ip: {}", ownIp);
         }
 
         List<EndpointDescription> allEndpoints = new ArrayList<>();
-        for (InetAddress ownIp : ownIps){
-            if (ownIp instanceof Inet4Address){
+        for (InetAddress ownIp : ownIps) {
+            if (ownIp instanceof Inet4Address) {
                 List<Inet4Address> reachableHosts = getReachableHosts(ownIp, DEFAULT_CIDR_SUFFIX);
-                for (Inet4Address reachableHost : reachableHosts){
+                for (Inet4Address reachableHost : reachableHosts) {
                     allEndpoints.addAll(tryToGetEndpoints(reachableHost));
                 }
             }
@@ -50,19 +50,19 @@ public class ScanningClient {
         // TODO report results
     }
 
-    private static List<EndpointDescription> tryToGetEndpoints(Inet4Address reachableHost){
+    private static List<EndpointDescription> tryToGetEndpoints(Inet4Address reachableHost) {
         List<EndpointDescription> endpointList = new ArrayList<>();
         logger.info("Trying to get endpoints for reachable host {}", reachableHost);
         EndpointDescription[] endpoints = new EndpointDescription[0];
-        try{
+        try {
             endpoints = UaTcpStackClient.getEndpoints(reachableHost.getHostAddress()).get();
         } catch (InterruptedException e) {
             logger.info("Interrupted Exception");
         } catch (ExecutionException e) {
             logger.info("Execution Exception");
         }
-        for (EndpointDescription endpoint : endpoints){
-            logger.info("Endpoint {}"+endpoint.getEndpointUrl());
+        for (EndpointDescription endpoint : endpoints) {
+            logger.info("Endpoint {}" + endpoint.getEndpointUrl());
             endpointList.add(endpoint);
         }
         return endpointList;
@@ -83,7 +83,7 @@ public class ScanningClient {
                     continue;
                 }
             } catch (SocketException e) {
-                logger.info("Socket exception for network interface "+ netint.getDisplayName());
+                logger.info("Socket exception for network interface " + netint.getDisplayName());
                 continue;
             }
             Enumeration<InetAddress> inetAddresses = netint.getInetAddresses();
@@ -102,25 +102,34 @@ public class ScanningClient {
      * For example, 131.234.44.70/24 will scan hosts whose ip address starts with 131.234.44., so 256 hosts.
      * Accordingly, 131.234.44.70/16 will scan hosts whose ip address starts with 131.234., so 256*256.
      * The runtime of the scan will change accordingly.
-     * @param ownIP The IP of this host
+     *
+     * @param ownIP      The IP of this host
      * @param cidrSuffix The
      * @return A list of addresses including all hosts which could be reached
      */
-    private static List<Inet4Address> getReachableHosts(InetAddress ownIP, int cidrSuffix){
+    private static List<Inet4Address> getReachableHosts(InetAddress ownIP, int cidrSuffix) {
         List<Inet4Address> reachableHosts = new ArrayList<>();
         String ipAddress = ownIP.toString();
         ipAddress = ipAddress.substring(1, ipAddress.lastIndexOf('.')) + ".";
         for (int i = 0; i < 256; i++) {
             String otherAddress = ipAddress + String.valueOf(i);
             try {
-                if (InetAddress.getByName(otherAddress).isReachable(50)) {
-                    reachableHosts.add((Inet4Address)InetAddress.getByName(otherAddress));
+                if (isPortOpen(otherAddress, OPCUA_DEFAULT_PORT)) {
+                    reachableHosts.add((Inet4Address) InetAddress.getByName(otherAddress));
                 }
             } catch (IOException e) {
-                e.printStackTrace();
+                logger.debug(e.getMessage());
             }
         }
         return reachableHosts;
     }
-}
 
+    public static boolean isPortOpen(String ipAddress, int port) {
+        try (Socket socket = new Socket()){
+            socket.connect(new InetSocketAddress(ipAddress, port), DEFAULT_TIMEOUT_IN_MS);
+        } catch (Exception e) {
+            return false;
+        }
+        return true;
+    }
+}
