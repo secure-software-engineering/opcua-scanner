33682fc4483c9698127241c315ec054a22ef1163
Melanie Bruns <mbruns42@mail.uni-paderborn.de>
2018-04-20 14:09:59 +0200

    Move networking part into separate class for easier reading

---
+++ src/de/fraunhofer/iem/opcuascanner/NetworkUtil.java
@@ -0,0 +1,99 @@
+package de.fraunhofer.iem.opcuascanner;
+
+import org.apache.commons.net.util.SubnetUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.net.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Enumeration;
+import java.util.List;
+
+class NetworkUtil {
+
+    private static final int OPCUA_DEFAULT_PORT = 4840;
+    private static final int DEFAULT_TIMEOUT_IN_MS = 500;
+
+    private static final Logger logger = LoggerFactory.getLogger(NetworkUtil.class);
+
+    private NetworkUtil(){
+        //private constructor since utility class should not be instantiated
+    }
+
+    static List<InetAddress> getOwnIpAddresses() {
+        List<InetAddress> ownInetAddresses = new ArrayList<>();
+        Enumeration<NetworkInterface> nets;
+        try {
+            nets = NetworkInterface.getNetworkInterfaces();
+        } catch (SocketException e) {
+            logger.error("Network interfaces could not be obtained.");
+            return ownInetAddresses;
+        }
+        for (NetworkInterface netInt : Collections.list(nets)) {
+            boolean netAvailable = true;
+            try {
+                if (!netInt.isUp() || netInt.isLoopback() || netInt.isVirtual()) {
+                    netAvailable=false;
+                }
+            } catch (SocketException e) {
+                logger.info("Socket exception for network interface " + netInt.getDisplayName());
+                netAvailable = false;
+            }
+            if (netAvailable){
+                Enumeration<InetAddress> inetAddresses = netInt.getInetAddresses();
+                ownInetAddresses.addAll(Collections.list(inetAddresses));
+            }
+        }
+        return ownInetAddresses;
+    }
+
+    /**
+     * Scans a subnet for reachable hosts depending on the ip and the cidrSuffix, which signifies unchanged bits from
+     * the start of the ip address.
+     * For example, 131.234.44.70/24 will scan hosts whose ip address starts with 131.234.44., so 256 hosts.
+     * Accordingly, 131.234.44.70/16 will scan hosts whose ip address starts with 131.234., so 256*256.
+     * The runtime of the scan will change accordingly.
+     *
+     * @param ownIP      The IP of this host
+     * @param cidrSuffix The
+     * @return A list of addresses including all hosts which could be reached
+     */
+    static List<Inet4Address> getReachableHosts(InetAddress ownIP, int cidrSuffix) {
+        List<Inet4Address> reachableHosts = new ArrayList<>();
+
+        SubnetUtils utils = new SubnetUtils(ownIP.getHostAddress()+"/"+cidrSuffix);
+        SubnetUtils.SubnetInfo info = utils.getInfo();
+        logger.info("Total usable addresses: {}", info.getAddressCountLong());
+
+        for (String otherAddress : info.getAllAddresses()) {
+            logger.info("Trying to reach host {}", otherAddress);
+            try {
+                if (isPortOpen(otherAddress, OPCUA_DEFAULT_PORT)) {
+                    reachableHosts.add((Inet4Address) InetAddress.getByName(otherAddress));
+                }
+            } catch (IOException e) {
+                logger.debug(e.getMessage());
+            }
+        }
+        return reachableHosts;
+    }
+
+    /**
+     * Checks whether the given port is open on the given ip address by trying to connect
+     * to it by socket. Uses the {@link NetworkUtil#DEFAULT_TIMEOUT_IN_MS} and reports the port as not
+     * open if the timeout is exceeded.
+     * @param ipAddress The ip address to which to try to connect
+     * @param port The port on which to connect to the host with the ip address
+     * @return true if a socket connection could be opened, else false
+     */
+    public static boolean isPortOpen(String ipAddress, int port) {
+        try (Socket socket = new Socket()){
+            socket.connect(new InetSocketAddress(ipAddress, port), DEFAULT_TIMEOUT_IN_MS);
+        } catch (Exception e) {
+            return false;
+        }
+        return true;
+    }
+}
--- src/de/fraunhofer/iem/opcuascanner/ScanningClient.java
+++ src/de/fraunhofer/iem/opcuascanner/ScanningClient.java
@@ -1,27 +1,27 @@
 package de.fraunhofer.iem.opcuascanner;
 
-import org.apache.commons.net.util.SubnetUtils;
+import org.eclipse.milo.opcua.sdk.client.OpcUaClient;
+import org.eclipse.milo.opcua.sdk.client.api.config.OpcUaClientConfig;
 import org.eclipse.milo.opcua.stack.client.UaTcpStackClient;
 import org.eclipse.milo.opcua.stack.core.types.structured.EndpointDescription;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.IOException;
-import java.net.*;
-import java.util.*;
+import java.net.Inet4Address;
+import java.net.InetAddress;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ScanningClient {
 
-    private static final int OPCUA_DEFAULT_PORT = 4840;
-    private static final int DEFAULT_CIDR_SUFFIX = 24;
-    private static final int DEFAULT_TIMEOUT_IN_MS = 500;
+    private static final int DEFAULT_CIDR_SUFFIX = 26;
 
     private static final Logger logger = LoggerFactory.getLogger(ScanningClient.class);
 
     public static void main(String[] args) {
         logger.info("Scanner started");
 
-        List<InetAddress> ownIps = getOwnIpAddresses();
+        List<InetAddress> ownIps = NetworkUtil.getOwnIpAddresses();
         for (InetAddress ownIp : ownIps) {
             logger.info("Own ip: {}", ownIp);
         }
@@ -29,7 +29,8 @@ public class ScanningClient {
         List<Inet4Address> reachableHosts = new ArrayList<>();
         for (InetAddress ownIp : ownIps) {
             if (ownIp instanceof Inet4Address) {
-                reachableHosts.addAll(getReachableHosts(ownIp, DEFAULT_CIDR_SUFFIX));
+                List<Inet4Address> reachableHostsForIP = NetworkUtil.getReachableHosts(ownIp, DEFAULT_CIDR_SUFFIX);
+                reachableHosts.addAll(reachableHostsForIP);
             }
         }
 
@@ -38,8 +39,15 @@ public class ScanningClient {
             allEndpoints.addAll(tryToGetEndpoints(reachableHost));
         }
 
-
         //TODO run client for each
+        for (EndpointDescription endpoint : allEndpoints){
+            OpcUaClientConfig config = OpcUaClientConfig.builder()
+                    .setEndpoint(endpoint)
+                    .build();
+
+            OpcUaClient client = new OpcUaClient(config);
+
+        }
 
         //TODO second phase: Try to set up connection anonymously
 
@@ -64,77 +72,5 @@ public class ScanningClient {
             endpointList.add(endpoint);
         }
         return endpointList;
-    }
-
-    private static List<InetAddress> getOwnIpAddresses() {
-        List<InetAddress> ownInetAddresses = new ArrayList<>();
-        Enumeration<NetworkInterface> nets;
-        try {
-            nets = NetworkInterface.getNetworkInterfaces();
-        } catch (SocketException e) {
-            logger.error("Network interfaces could not be obtained.");
-            return ownInetAddresses;
-        }
-        for (NetworkInterface netInt : Collections.list(nets)) {
-            try {
-                if (!netInt.isUp() || netInt.isLoopback() || netInt.isVirtual()) {
-                    continue;
-                }
-            } catch (SocketException e) {
-                logger.info("Socket exception for network interface " + netInt.getDisplayName());
-                continue;
-            }
-            Enumeration<InetAddress> inetAddresses = netInt.getInetAddresses();
-            ownInetAddresses.addAll(Collections.list(inetAddresses));
-        }
-        return ownInetAddresses;
-    }
-
-    /**
-     * Scans a subnet for reachable hosts depending on the ip and the cidrSuffix, which signifies unchanged bits from
-     * the start of the ip address.
-     * For example, 131.234.44.70/24 will scan hosts whose ip address starts with 131.234.44., so 256 hosts.
-     * Accordingly, 131.234.44.70/16 will scan hosts whose ip address starts with 131.234., so 256*256.
-     * The runtime of the scan will change accordingly.
-     *
-     * @param ownIP      The IP of this host
-     * @param cidrSuffix The
-     * @return A list of addresses including all hosts which could be reached
-     */
-    private static List<Inet4Address> getReachableHosts(InetAddress ownIP, int cidrSuffix) {
-        List<Inet4Address> reachableHosts = new ArrayList<>();
-
-        SubnetUtils utils = new SubnetUtils(ownIP.getHostAddress()+"/"+cidrSuffix);
-        SubnetUtils.SubnetInfo info = utils.getInfo();
-        logger.info("Total usable addresses: {}", info.getAddressCountLong());
-
-        for (String otherAddress : info.getAllAddresses()) {
-            logger.info("Trying to reach host {}", otherAddress);
-            try {
-                if (isPortOpen(otherAddress, OPCUA_DEFAULT_PORT)) {
-                    reachableHosts.add((Inet4Address) InetAddress.getByName(otherAddress));
-                }
-            } catch (IOException e) {
-                logger.debug(e.getMessage());
-            }
-        }
-        return reachableHosts;
-    }
-
-    /**
-     * Checks whether the given port is open on the given ip address by trying to connect
-     * to it by socket. Uses the {@link ScanningClient#DEFAULT_TIMEOUT_IN_MS} and reports the port as not
-     * open if the timeout is exceeded.
-     * @param ipAddress The ip address to which to try to connect
-     * @param port The port on which to connect to the host with the ip address
-     * @return true if a socket connection could be opened, else false
-     */
-    public static boolean isPortOpen(String ipAddress, int port) {
-        try (Socket socket = new Socket()){
-            socket.connect(new InetSocketAddress(ipAddress, port), DEFAULT_TIMEOUT_IN_MS);
-        } catch (Exception e) {
-            return false;
-        }
-        return true;
     }
 }
