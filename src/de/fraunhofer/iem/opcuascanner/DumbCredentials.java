52306e7322bd7b91a635ac7d05e648de65e96652
Melanie Bruns <mbruns42@mail.uni-paderborn.de>
2018-05-12 10:23:09 +0200

    Statically initialize list of dumb logins

--- src/de/fraunhofer/iem/opcuascanner/DumbCredentials.java
+++ src/de/fraunhofer/iem/opcuascanner/DumbCredentials.java
@@ -3,9 +3,21 @@ package de.fraunhofer.iem.opcuascanner;
 import java.util.ArrayList;
 import java.util.List;
 
-public class DumbCredentials {
+/**
+ * Class providing logins that can be regarded as insecure since
+ * they may be easy to guess.
+ * Loosely based on most recent data from https://en.wikipedia.org/wiki/List_of_the_most_common_passwords.
+ * Also includes passwords from eclipse milo examples.
+ */
+class DumbCredentials {
+
+    private DumbCredentials() {
+        //Do not instantiate this, this a util class.
+        //Private constructor hides implicit public one
+    }
+
     static final List<Login> logins = new ArrayList<>();
-    public DumbCredentials(){
+    static{
         logins.add(new Login("username", "password"));
         logins.add(new Login("user", "password"));
         logins.add(new Login("username", "password1"));
