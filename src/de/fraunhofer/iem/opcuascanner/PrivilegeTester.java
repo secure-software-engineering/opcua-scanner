27a733101c51671274bc1347b5c0255129941153
Melanie Bruns <mbruns42@mail.uni-paderborn.de>
2018-05-17 11:01:26 +0200

    Try writing

--- src/de/fraunhofer/iem/opcuascanner/PrivilegeTester.java
+++ src/de/fraunhofer/iem/opcuascanner/PrivilegeTester.java
@@ -1,10 +1,18 @@
 package de.fraunhofer.iem.opcuascanner;
 
+import com.google.common.collect.ImmutableList;
 import de.fraunhofer.iem.opcuascanner.logic.AccessPrivileges;
 import de.fraunhofer.iem.opcuascanner.logic.Authentication;
 import de.fraunhofer.iem.opcuascanner.logic.Privilege;
 import de.fraunhofer.iem.opcuascanner.utils.OpcuaUtil;
 import org.eclipse.milo.opcua.sdk.client.OpcUaClient;
+import org.eclipse.milo.opcua.stack.core.types.builtin.DataValue;
+import org.eclipse.milo.opcua.stack.core.types.builtin.NodeId;
+import org.eclipse.milo.opcua.stack.core.types.builtin.StatusCode;
+import org.eclipse.milo.opcua.stack.core.types.builtin.Variant;
+
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
 
 import static java.lang.Thread.sleep;
 
@@ -29,12 +37,29 @@ class PrivilegeTester {
         try{
             client.connect().get();
             privileges.setPrivilegePerAuthenticationToTrue(Privilege.CONNECT, auth);
+
             //Now try to read
             privileges.privilegeWasTestedPerAuthentication(Privilege.READ, auth);
             OpcuaUtil.readServerStateAndTime(client).thenAccept(values -> 
                     privileges.setPrivilegePerAuthenticationToTrue(Privilege.READ, auth));
             //Give the client some time to read
             sleep(50);
+
+            //Now try to write
+            privileges.privilegeWasTestedPerAuthentication(Privilege.WRITE, auth);
+            List<NodeId> nodeIds = ImmutableList.of(new NodeId(2, "HelloWorld/ScalarTypes/Int32"));
+            Variant v = new Variant(0);
+            DataValue dv = new DataValue(v, null, null);
+
+            // write asynchronously....
+            CompletableFuture<List<StatusCode>> f = client.writeValues(nodeIds, ImmutableList.of(dv));
+
+            // ...but block for the result
+            StatusCode status = f.get().get(0);
+            if (status.isGood()) {
+                privileges.setPrivilegePerAuthenticationToTrue(Privilege.WRITE, auth);
+            }
+
         }
         catch (Exception e){
             //If we can't connect that's fine
