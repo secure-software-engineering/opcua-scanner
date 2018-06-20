6e12f7c8876211f240210fcd18467141b74a3540
Melanie Bruns <mbruns42@mail.uni-paderborn.de>
2018-06-20 16:11:07 +0200

    Add new authentication type

--- src/main/java/de/fraunhofer/iem/opcuascanner/logic/Authentication.java
+++ src/main/java/de/fraunhofer/iem/opcuascanner/logic/Authentication.java
@@ -4,5 +4,5 @@ package de.fraunhofer.iem.opcuascanner.logic;
  * Ways to authenticate when connecting to a server
  */
 public enum Authentication {
-    ANONYMOUSLY, COMMON_CREDENTIALS, EXPIRED_CERTIFICATE
+    ANONYMOUSLY, COMMON_CREDENTIALS, EXPIRED_CERTIFICATE, CERTIFICATE_NOT_VALID_YET,
 }
