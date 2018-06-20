d188b6aa7c62a73205370330a0246f285bc5779e
Melanie Bruns <mbruns42@mail.uni-paderborn.de>
2018-06-20 20:04:30 +0200

    Add authentication kind for certificate that contains the wrong hostname

--- src/main/java/de/fraunhofer/iem/opcuascanner/logic/Authentication.java
+++ src/main/java/de/fraunhofer/iem/opcuascanner/logic/Authentication.java
@@ -4,5 +4,5 @@ package de.fraunhofer.iem.opcuascanner.logic;
  * Ways to authenticate when connecting to a server
  */
 public enum Authentication {
-    ANONYMOUSLY, COMMON_CREDENTIALS, EXPIRED_CERTIFICATE, CERTIFICATE_NOT_VALID_YET,
+    ANONYMOUSLY, COMMON_CREDENTIALS, EXPIRED_CERTIFICATE, CERTIFICATE_NOT_VALID_YET, CERTIFICATE_WRONG_HOSTNAME,
 }
