9ed03085859baf4df63957bf1d5710459beea02b
Melanie Bruns <mbruns42@mail.uni-paderborn.de>
2018-06-20 21:14:39 +0200

    Add another authentication method for certificate with wrong key usage

--- src/main/java/de/fraunhofer/iem/opcuascanner/logic/Authentication.java
+++ src/main/java/de/fraunhofer/iem/opcuascanner/logic/Authentication.java
@@ -4,5 +4,10 @@ package de.fraunhofer.iem.opcuascanner.logic;
  * Ways to authenticate when connecting to a server
  */
 public enum Authentication {
-    ANONYMOUSLY, COMMON_CREDENTIALS, EXPIRED_CERTIFICATE, CERTIFICATE_NOT_VALID_YET, CERTIFICATE_WRONG_HOSTNAME,
+    ANONYMOUSLY,
+    COMMON_CREDENTIALS,
+    EXPIRED_CERTIFICATE,
+    CERTIFICATE_NOT_VALID_YET,
+    CERTIFICATE_WRONG_HOSTNAME,
+    CERTIFICATE_WRONG_KEY_USAGE
 }
