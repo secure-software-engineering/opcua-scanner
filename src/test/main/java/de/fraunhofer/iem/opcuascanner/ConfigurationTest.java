7054bda65446d515ae483860607502b65a0f472c
Melanie Bruns <mbruns42@mail.uni-paderborn.de>
2018-06-26 12:27:23 +0200

    Test port configuration and make testConfig private

--- src/test/main/java/de/fraunhofer/iem/opcuascanner/ConfigurationTest.java
+++ src/test/main/java/de/fraunhofer/iem/opcuascanner/ConfigurationTest.java
@@ -10,9 +10,10 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 public class ConfigurationTest {
-    static final String testOutputFilename = "testOutputFilename";
+    private static final String TEST_OUTPUT_FILENAME = "testOutputFilename";
+    private static final int TEST_PORT = 42;
 
-    File testConfig;
+    private File testConfig;
 
     @Before
     public void loadTestConfiguation(){
@@ -48,9 +49,16 @@ public class ConfigurationTest {
         if (testConfig != null){
             Configuration.tryToLoadConfigFile(testConfig);
         }
-        assertEquals("Output file name incorrect.", testOutputFilename, Configuration.getOutputFileName());
+        assertEquals("Output file name incorrect.", TEST_OUTPUT_FILENAME, Configuration.getOutputFileName());
     }
 
+    @Test
+    public void testPort(){
+        if (testConfig != null){
+            Configuration.tryToLoadConfigFile(testConfig);
+        }
+        assertEquals("Port was incorrect.", TEST_PORT, Configuration.getPort());
+    }
 }
 
 
