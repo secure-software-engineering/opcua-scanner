6e5a52512b6529f8cfce40e89b564672540e83b9
Melanie Bruns <mbruns42@mail.uni-paderborn.de>
2018-06-26 16:01:03 +0200

    Test getIpAddresses for ip ranges and stick to small range

--- src/test/main/java/de/fraunhofer/iem/opcuascanner/ConfigurationTest.java
+++ src/test/main/java/de/fraunhofer/iem/opcuascanner/ConfigurationTest.java
@@ -6,7 +6,7 @@ import org.junit.Test;
 import java.io.File;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
-import java.util.Set;
+import java.util.*;
 
 import static org.junit.Assert.*;
 
@@ -14,9 +14,10 @@ public class ConfigurationTest {
     private static final String TEST_OUTPUT_FILENAME = "testOutputFilename";
     private static final int TEST_PORT = 42;
     private static final String TEST_HOSTNAME = "fraunhofer.de";
-    private static final String TEST_IP_RANGE_START = "8.8.8.8";
-    private static final int TEST_IP_RANGE_END = 20;
-    private static final String TEST_IP_CIDR_BASE = "192.03.134.7";
+    private static final String TEST_IP_RANGE_BASE = "8.8.8.";
+    private static final int TEST_IP_RANGE_START = 8; //NOSONAR
+    private static final int TEST_IP_RANGE_END = 11;
+    private static final String TEST_IP_CIDR_BASE = "192.03.134.7"; //NOSONAR
     private static final int TEST_IP_CIDR_SUFFIX = 29;
 
     private File testConfig;
@@ -24,45 +25,31 @@ public class ConfigurationTest {
     @Before
     public void loadTestConfiguation(){
         testConfig = new File("src/test/resources/test_config.txt");
+        Configuration.tryToLoadConfigFile(testConfig);
     }
 
     @Test
     public void testCallActivated(){
-        if (testConfig != null){
-            Configuration.tryToLoadConfigFile(testConfig);
-        }
         assertTrue("Call should be activated.", Configuration.isCallActivated());
     }
 
     @Test
     public void testDeleteNotActivated(){
-        if (testConfig != null){
-            Configuration.tryToLoadConfigFile(testConfig);
-        }
         assertFalse("Delete should not be activated.", Configuration.isDeleteActivated());
     }
 
     @Test
     public void testWriteActivated(){
-        if (testConfig != null){
-            Configuration.tryToLoadConfigFile(testConfig);
-        }
         assertTrue("Write should be activated.", Configuration.isWriteActivated());
     }
 
     @Test
     public void testOutputFileName(){
-        if (testConfig != null){
-            Configuration.tryToLoadConfigFile(testConfig);
-        }
         assertEquals("Output file name incorrect.", TEST_OUTPUT_FILENAME, Configuration.getOutputFileName());
     }
 
     @Test
     public void testPort(){
-        if (testConfig != null){
-            Configuration.tryToLoadConfigFile(testConfig);
-        }
         assertEquals("Port was incorrect.", TEST_PORT, Configuration.getPort());
     }
 
@@ -90,6 +77,32 @@ public class ConfigurationTest {
                     hostFoundInIpAddresses = true;
             }
             assertTrue("Host should be contained in ip addresses.", hostFoundInIpAddresses);
+        }
+    }
+
+    @Test
+    public void testIpRangeInIpAddresses(){
+        boolean testCouldSucceed = true;
+        Map<String, Boolean> exptectedAddressIncluded = new HashMap<>();
+        try{
+            for (int lastNumInIp = TEST_IP_RANGE_START; lastNumInIp <= TEST_IP_RANGE_END; lastNumInIp++){
+                String ip = TEST_IP_RANGE_BASE + lastNumInIp;
+                exptectedAddressIncluded.put(InetAddress.getByName(ip).toString(), false);
+            }
+        } catch (UnknownHostException e) {
+            testCouldSucceed = false;
+        }
+        if (testCouldSucceed ){
+            Set<InetAddress> inetAddressSet = Configuration.getIpAddresses();
+            for (InetAddress inetAddress : inetAddressSet){
+                if (exptectedAddressIncluded.containsKey(inetAddress.toString()))
+                    exptectedAddressIncluded.put(inetAddress.toString(), true);
+            }
+            for (Map.Entry<String,Boolean> expectedAddress : exptectedAddressIncluded.entrySet()){
+                assertTrue("Host "+ expectedAddress.getKey() + " should be contained in ip addresses.",
+                        expectedAddress.getValue());
+            }
+
         }
     }
 }
