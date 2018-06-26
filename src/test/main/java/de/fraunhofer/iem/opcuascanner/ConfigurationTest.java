d718c2c5de08a34d45b3bcd3221ec127ea0695cc
Melanie Bruns <mbruns42@mail.uni-paderborn.de>
2018-06-26 15:29:55 +0200

    Test getIpAddresses generally and for a URL

--- src/test/main/java/de/fraunhofer/iem/opcuascanner/ConfigurationTest.java
+++ src/test/main/java/de/fraunhofer/iem/opcuascanner/ConfigurationTest.java
@@ -4,14 +4,20 @@ import org.junit.Before;
 import org.junit.Test;
 
 import java.io.File;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.Set;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 public class ConfigurationTest {
     private static final String TEST_OUTPUT_FILENAME = "testOutputFilename";
     private static final int TEST_PORT = 42;
+    private static final String TEST_HOSTNAME = "fraunhofer.de";
+    private static final String TEST_IP_RANGE_START = "8.8.8.8";
+    private static final int TEST_IP_RANGE_END = 20;
+    private static final String TEST_IP_CIDR_BASE = "192.03.134.7";
+    private static final int TEST_IP_CIDR_SUFFIX = 29;
 
     private File testConfig;
 
@@ -58,6 +64,33 @@ public class ConfigurationTest {
             Configuration.tryToLoadConfigFile(testConfig);
         }
         assertEquals("Port was incorrect.", TEST_PORT, Configuration.getPort());
+    }
+
+    @Test
+    public void testGetIpAddressesReturnsIpAddresses(){
+        Set<InetAddress> inetAddressSet = Configuration.getIpAddresses();
+        assertNotNull("Ip addresses should not be null", inetAddressSet);
+        assertFalse("Ip addresses should not be empty", inetAddressSet.isEmpty());
+    }
+
+    @Test
+    public void testHostInIpAddresses(){
+        boolean testCouldSucceed = true;
+        InetAddress fraunhofer = null;
+        try{
+            fraunhofer = InetAddress.getByName(TEST_HOSTNAME);
+        } catch (UnknownHostException e) {
+            testCouldSucceed = false;
+        }
+        if (testCouldSucceed && fraunhofer != null){
+            Set<InetAddress> inetAddressSet = Configuration.getIpAddresses();
+            boolean hostFoundInIpAddresses = false;
+            for (InetAddress inetAddress : inetAddressSet){
+                if (inetAddress.equals(fraunhofer))
+                    hostFoundInIpAddresses = true;
+            }
+            assertTrue("Host should be contained in ip addresses.", hostFoundInIpAddresses);
+        }
     }
 }
 
